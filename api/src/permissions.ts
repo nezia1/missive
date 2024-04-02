335f3fb818410865a15f17b5eb038caae4c1a81b
Anthony Rodriguez <anthony@nezia.dev>
2024-04-02 12:16:04 +0200

    delete key after being found in GET and add POST

--- api/src/permissions.ts
+++ api/src/permissions.ts
@@ -2,4 +2,5 @@ export enum Permissions {
 	PROFILE_READ = 'profile:read',
 	PROFILE_WRITE = 'profile:write',
 	KEYS_READ = 'keys:read',
+	KEYS_WRITE = 'keys:write',
 }
--- api/src/routes/keys.ts
+++ api/src/routes/keys.ts
@@ -1,13 +1,14 @@
 import type { APIReply, UserParams } from '@/globals'
 import { authenticationHook, authorizationHook } from '@/hooks'
 import { Permissions } from '@/permissions'
-import { PrismaClient } from '@prisma/client'
+import { type Prisma, PrismaClient } from '@prisma/client'
 import type { FastifyPluginCallback } from 'fastify'
 
 const prisma = new PrismaClient()
 const secret = new TextEncoder().encode(process.env.JWT_SECRET)
 
 const keys: FastifyPluginCallback = (fastify, _, done) => {
+	// Gets the first pre key for a user, as well as the signed pre key
 	fastify.route<{ Reply: APIReply; Params: UserParams }>({
 		method: 'GET',
 		url: '/:id/keys',
@@ -16,14 +17,45 @@ const keys: FastifyPluginCallback = (fastify, _, done) => {
 			authorizationHook([Permissions.KEYS_READ]),
 		],
 		handler: async (request, reply) => {
-			const keys = await prisma.preKey.findMany({
+			const preKey = await prisma.preKey.findFirst({
 				where: {
 					userId: request.params.id,
 				},
 			})
-			reply.status(200).send({ data: { keys } })
+
+			// TODO: handle case when preKey is not found (we are just going to not delete and return null for now to avoid it crashing)
+			if (preKey)
+				await prisma.preKey.delete({
+					where: {
+						id: preKey?.id,
+					},
+				})
+
+			const signedPreKey = await prisma.signedPreKey.findFirst({
+				where: { userId: request.params.id },
+			})
+
+			reply.status(200).send({ data: { preKey, signedPreKey } })
 		},
 	})
+
+	fastify.route<{
+		Body: Prisma.PreKeyCreateManyInput[]
+		Reply: APIReply
+		Params: UserParams
+	}>({
+		method: 'POST',
+		url: '/:id/keys/pre',
+		preParsing: [
+			authenticationHook,
+			authorizationHook([Permissions.KEYS_WRITE]),
+		],
+		handler: async (request, reply) => {
+			await prisma.preKey.createMany({ data: request.body })
+			reply.status(204).send()
+		},
+	})
+
 	done()
 }
 
