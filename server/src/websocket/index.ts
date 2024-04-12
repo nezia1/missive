342beceb8426b55d5bc440d0997c3f3f8f7a2908
Anthony Rodriguez <anthony@nezia.dev>
2024-04-12 15:53:56 +0200

    start implementing real-time communication

--- server/src/websocket/index.ts
+++ server/src/websocket/index.ts
@@ -1,6 +1,7 @@
-import type { Prisma, PrismaClient } from '@prisma/client'
+import type { PendingMessage, Prisma, PrismaClient } from '@prisma/client'
 import type { FastifyPluginCallback } from 'fastify'
 
+import { randomUUID } from 'node:crypto'
 import { authenticationHook } from '@/hooks'
 import { parseGenericError } from '@/utils'
 
@@ -8,15 +9,83 @@ interface UserMessage {
 	userId: string
 	content: string
 }
+
+enum MessageStatus {
+	SENT = 'sent',
+	DELIVERED = 'delivered',
+	READ = 'read',
+	ERROR = 'error',
+}
+
+const connections = new Map<string, WebSocket>()
 const websocket: FastifyPluginCallback = (fastify, _, done) => {
 	fastify.addHook('preParsing', authenticationHook)
 	fastify.get('/', { websocket: true }, (socket, req) => {
-		socket.on('message', (msg) => {
+		if (!req.authenticatedUser)
+			return socket.close(1008, 'User is not authenticated')
+
+		connections.set(req.authenticatedUser.id, socket)
+
+		socket.on('message', async (msg) => {
+			if (!req.authenticatedUser) return
+
 			// TODO: handle JSON parsing error (if it's not a valid JSON string, it will crash the conneection)
 			const message = JSON.parse(
 				msg.toString(),
-			) as Prisma.PendingMessageCreateInput
-			socket.send(`WEBSOCKET IS WORKING 🎉!  User sent ${message.content}`)
+			) as Prisma.PendingMessageCreateManyInput
+
+			// Create a pending message
+			const pendingMessage: Prisma.PendingMessageCreateManyInput = {
+				content: message.content,
+				receiverId: message.receiverId,
+				senderId: req.authenticatedUser.id,
+			}
+
+			socket.send(
+				JSON.stringify({
+					status: MessageStatus.SENT,
+					message: pendingMessage,
+				}),
+			)
+
+			if (connections.has(message.receiverId)) {
+				const receiver = connections.get(message.receiverId)
+				receiver?.send(JSON.stringify(pendingMessage))
+				socket.send(
+					JSON.stringify({
+						status: MessageStatus.DELIVERED,
+						message: pendingMessage,
+					}),
+				)
+			} else {
+				// Check if receiver exists
+				const offlineReceiver = await fastify.prisma.user.findUnique({
+					where: {
+						id: message.receiverId,
+					},
+				})
+
+				if (!offlineReceiver)
+					return socket.send(
+						JSON.stringify({
+							status: MessageStatus.ERROR,
+							error: 'Receiver not found',
+						}),
+					)
+
+				fastify.prisma.pendingMessage
+					.create({
+						data: pendingMessage,
+					})
+					.then((storedPendingMessage) => {
+						// TODO send push notification to the receiver. If successful, update the message status to delivered for the sender.
+					})
+			}
+		})
+
+		socket.on('close', () => {
+			if (!req.authenticatedUser) return
+			connections.delete(req.authenticatedUser.id)
 		})
 	})
 
