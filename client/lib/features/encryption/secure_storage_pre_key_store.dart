770a50b37b1fe1c8a0d1c51d968422b86598b5ff
Anthony Rodriguez <anthony@nezia.dev>
2024-04-18 09:22:51 +0200

    move keys loading and parsing to its own private method

--- client/lib/features/encryption/secure_storage_pre_key_store.dart
+++ client/lib/features/encryption/secure_storage_pre_key_store.dart
@@ -18,18 +18,16 @@ class SecureStoragePreKeyStore implements PreKeyStore {
   /// Loads a serialized [PreKeyRecord] from [FlutterSecureStorage] and returns it as a [PreKeyRecord].
   /// Throws a n [Exception] if the pre key is not found.
   Future<PreKeyRecord> loadPreKey(int preKeyId) async {
-    final preKeysJson = await _secureStorage.read(key: 'preKeys');
+    final preKeys = await _loadKeys();
 
-    if (preKeysJson == null) throw Exception('PreKeys not found');
+    final preKey = preKeys[preKeyId.toString()];
 
-    Map<String, dynamic> preKeys = jsonDecode(preKeysJson);
-
-    if (!preKeys.containsKey(preKeyId.toString())) {
+    if (preKey == null) {
       throw Exception('PreKey not found');
     }
 
     // Decode the base64 string and deserialize it as a PreKeyRecord
-    return PreKeyRecord.fromBuffer(base64Decode(preKeys[preKeyId.toString()]));
+    return PreKeyRecord.fromBuffer(base64Decode(preKey));
   }
 
   @override
@@ -43,12 +41,22 @@ class SecureStoragePreKeyStore implements PreKeyStore {
   /// Store a [PreKeyRecord] in the device's secure storage
   /// The pre key is stored as a base64 encoded [String]
   Future<void> storePreKey(int preKeyId, PreKeyRecord record) async {
-    final preKeysJson = await _secureStorage.read(key: 'preKeys');
-    Map<String, dynamic> preKeys =
-        preKeysJson != null ? jsonDecode(preKeysJson) : {};
+    final preKeys = await _loadKeys();
 
     // Serialize the record as a UInt8List and encode it as a base64 string (SecureStorage expects strings. This will need to be decoded when retrieved)
     preKeys[preKeyId.toString()] = base64Encode(record.serialize());
+
     await _secureStorage.write(key: 'preKeys', value: jsonEncode(preKeys));
+  }
+
+  /// Loads all pre keys from the secure storage
+  /// Returns a [Map] of [PreKeyRecord]s
+  Future<Map<String, dynamic>> _loadKeys() async {
+    final preKeysJson = await _secureStorage.read(key: 'preKeys');
+    if (preKeysJson == null) throw Exception('PreKeys not found');
+
+    final preKeys = jsonDecode(preKeysJson);
+
+    return preKeys;
   }
 }
