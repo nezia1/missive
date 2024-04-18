fcf725d80a8f37bbdc21472c51313848a444015a
Anthony Rodriguez <anthony@nezia.dev>
2024-04-18 16:47:55 +0200

    implement all stores in install

--- client/lib/features/home/screens/home_screen.dart
+++ client/lib/features/home/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:missive/features/encryption/secure_storage_session_store.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -8,6 +9,7 @@ import 'package:libsignal_protocol_dart/libsignal_protocol_dart.dart';
 import 'package:missive/features/authentication/providers/auth_provider.dart';
 import 'package:missive/features/encryption/secure_storage_pre_key_store.dart';
 import 'package:missive/features/encryption/secure_storage_identity_key_store.dart';
+import 'package:missive/features/encryption/secure_storage_signed_pre_key_store.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key, required this.title});
@@ -21,35 +23,43 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   // late is needed here because AuthProvider requires context and IdentityKeyStore is initialized in initState (the constructor needs to be different whether or not the user just created their account)
   late AuthProvider _userProvider;
-  late IdentityKeyStore identityKeyStore;
-  final preKeyStore = SecureStoragePreKeyStore(const FlutterSecureStorage());
-
+  late SecureStorageIdentityKeyStore identityKeyStore;
   @override
   void initState() {
     super.initState();
     _userProvider = Provider.of<AuthProvider>(context, listen: false);
     SharedPreferences.getInstance().then((prefs) {
       // TODO: when changing accounts, this should also trigger. But not on log out, in case the user logs back and in again and it's the same account.
-      if (prefs.getBool('installed') == null) {
-        install();
-        return;
-      }
-      identityKeyStore =
-          SecureStorageIdentityKeyStore(const FlutterSecureStorage());
+      // if (prefs.getBool('installed') == null) {
+      install();
+      return;
+      // }
     });
   }
 
   void install() async {
+    const secureStorage = FlutterSecureStorage();
     final identityKeyPair = generateIdentityKeyPair();
     final registrationId = generateRegistrationId(false);
 
+    final signedPreKey = generateSignedPreKey(identityKeyPair, 0);
+
+    final preKeyStore = SecureStoragePreKeyStore(secureStorage);
+    final signedPreKeyStore = SecureStorageSignedPreKeyStore(secureStorage);
     identityKeyStore = SecureStorageIdentityKeyStore.fromIdentityKeyPair(
-        const FlutterSecureStorage(), identityKeyPair, registrationId);
+        secureStorage, identityKeyPair, registrationId);
+    final sessionStore = SecureStorageSessionStore(secureStorage);
+
     final preKeys = generatePreKeys(0, 110);
     for (var p in preKeys) {
       await preKeyStore.storePreKey(p.id, p);
     }
-    print((await preKeyStore.loadPreKey(0)).serialize());
+
+    await signedPreKeyStore.storeSignedPreKey(signedPreKey.id, signedPreKey);
+
+    final remoteAddress = SignalProtocolAddress('+1234567890', 1);
+    final SessionBuilder sessionBuilder = SessionBuilder(sessionStore,
+        preKeyStore, signedPreKeyStore, identityKeyStore, remoteAddress);
   }
 
   @override
