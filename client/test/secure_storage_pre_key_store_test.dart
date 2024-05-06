4bd9c42656a9b9ad943fb3af49460bd2ddb31c26
Anthony Rodriguez <anthony@nezia.dev>
2024-05-06 12:44:47 +0200

    add tests for pre key store

--- client/test/secure_storage_pre_key_store_test.dart
+++ client/test/secure_storage_pre_key_store_test.dart
@@ -21,5 +21,56 @@ void main() {
 
       expect(await preKeyStore.containsPreKey(0), isFalse);
     });
+
+    test(
+        'loadPreKey should throw an InvalidKeyIdException if there are no preKeys',
+        () async {
+      final secureStorage = MockSecureStorage();
+      final preKeyStore = SecureStoragePreKeyStore(secureStorage);
+
+      when(secureStorage.read(key: 'preKeys'))
+          .thenAnswer((_) => Future.value(null));
+
+      expect(() async => await preKeyStore.loadPreKey(0),
+          throwsA(isA<InvalidKeyIdException>()));
+    });
+
+    test(
+        'loadPreKey should throw an InvalidKeyIdException if the preKey is not found',
+        () async {
+      final secureStorage = MockSecureStorage();
+      final preKeyStore = SecureStoragePreKeyStore(secureStorage);
+
+      when(secureStorage.read(key: 'preKeys'))
+          .thenAnswer((_) => Future.value('{"1": "non-existent-key"}'));
+
+      expect(() async => await preKeyStore.loadPreKey(0),
+          throwsA(isA<InvalidKeyIdException>()));
+    });
+
+    test('removePreKey should do nothing if there are no preKeys', () async {
+      final secureStorage = MockSecureStorage();
+      final preKeyStore = SecureStoragePreKeyStore(secureStorage);
+
+      when(secureStorage.read(key: 'preKeys'))
+          .thenAnswer((_) => Future.value(null));
+
+      await preKeyStore.removePreKey(0);
+      verifyNever(
+          secureStorage.write(key: 'preKeys', value: anyNamed('value')));
+    });
+
+    test('storePreKey should store a preKey in the secure storage', () async {
+      final secureStorage = MockSecureStorage();
+      final preKeyStore = SecureStoragePreKeyStore(secureStorage);
+      final preKeyRecord = generatePreKeys(0, 1)[0];
+
+      when(secureStorage.read(key: 'preKeys'))
+          .thenAnswer((_) => Future.value(null));
+
+      await preKeyStore.storePreKey(0, preKeyRecord);
+
+      verify(secureStorage.write(key: 'preKeys', value: anyNamed('value')));
+    });
   });
 }
