efd36fbe97403c83b11ac7cb939e7f6b9300f210
Anthony Rodriguez <anthony@nezia.dev>
2024-05-06 15:35:45 +0200

    add unit tests for session store (containsSession, loadSession, storeSession)

--- client/test/secure_storage_session_store_test.dart
+++ client/test/secure_storage_session_store_test.dart
@@ -24,5 +24,83 @@ void main() {
               .containsSession(const SignalProtocolAddress('test', 1)),
           isFalse);
     });
+
+    test('containsSession should return true if there is a session', () async {
+      final secureStorage = MockSecureStorage();
+      final sessionStore = SecureStorageSessionStore(secureStorage);
+
+      when(secureStorage.read(key: 'sessions'))
+          .thenAnswer((_) => Future.value(jsonEncode({
+                'test': {
+                  '1': {
+                    'session': 'session',
+                    'created_at': DateTime.now().toIso8601String()
+                  }
+                }
+              })));
+
+      expect(
+          await sessionStore
+              .containsSession(const SignalProtocolAddress('test', 1)),
+          isTrue);
+    });
+
+    test('loadSession should return an empty session if it does not exist',
+        () async {
+      final secureStorage = MockSecureStorage();
+      final sessionStore = SecureStorageSessionStore(secureStorage);
+
+      when(secureStorage.read(key: 'sessions'))
+          .thenAnswer((_) => Future.value(null));
+
+      expect(
+          (await sessionStore
+                  .loadSession(const SignalProtocolAddress('test', 1)))
+              .isFresh(),
+          isTrue);
+    });
+
+    test('loadSession should return a session if there is one', () async {
+      final secureStorage = MockSecureStorage();
+      final sessionStore = SecureStorageSessionStore(secureStorage);
+
+      when(secureStorage.read(key: 'sessions'))
+          .thenAnswer((_) => Future.value(jsonEncode({
+                'test': {
+                  '1': {
+                    'session': 'session',
+                    'created_at': DateTime.now().toIso8601String()
+                  }
+                }
+              })));
+
+      final session = await sessionStore
+          .loadSession(const SignalProtocolAddress('test', 1));
+
+      expect(session, isNotNull);
+      expect(session, isA<SessionRecord>());
+      expect(session, isNot(session.isFresh()));
+    });
+  });
+
+  test('storeSession should store a session', () async {
+    final secureStorage = MockSecureStorage();
+    final sessionstore = SecureStorageSessionStore(secureStorage);
+
+    when(secureStorage.read(key: 'sessions'))
+        .thenAnswer((_) => Future.value(null));
+
+    await sessionstore.storeSession(
+        const SignalProtocolAddress('test', 1), SessionRecord());
+
+    verify(
+      secureStorage.write(
+          key: 'sessions',
+          // checks that the value is a json string with the correct key
+          value: argThat(predicate<String>((jsonString) {
+            final decoded = jsonDecode(jsonString);
+            return decoded['test:1'] != null && decoded['test:1'] is String;
+          }), named: 'value')),
+    ).called(1);
   });
 }
