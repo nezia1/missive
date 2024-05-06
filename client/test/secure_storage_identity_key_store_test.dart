222a97463ac4c4f3484cda9c3aa5c1e465bb39fe
Anthony Rodriguez <anthony@nezia.dev>
2024-05-06 12:36:53 +0200

    group tests

--- client/test/secure_storage_identity_key_store_test.dart
+++ client/test/secure_storage_identity_key_store_test.dart
@@ -10,142 +10,151 @@ import 'secure_storage_identity_key_store_test.mocks.dart';
 
 @GenerateMocks([SecureStorage])
 void main() {
-  test(
-      'SecureStorageIdentityKeyStore should store IdentityKeyPair and registrationId',
-      () async {
-    final secureStorage = MockSecureStorage();
-    final identityKeyPair = generateIdentityKeyPair();
-    final registrationId = generateRegistrationId(false);
-    SecureStorageIdentityKeyStore.fromIdentityKeyPair(
-        secureStorage, identityKeyPair, registrationId);
+  group('SecureStorageIdentityKeyStore', () {
+    test(
+        'SecureStorageIdentityKeyStore should store IdentityKeyPair and registrationId',
+        () async {
+      final secureStorage = MockSecureStorage();
+      final identityKeyPair = generateIdentityKeyPair();
+      final registrationId = generateRegistrationId(false);
+      SecureStorageIdentityKeyStore.fromIdentityKeyPair(
+          secureStorage, identityKeyPair, registrationId);
 
-    when(secureStorage.read(key: 'identityKeyPair')).thenAnswer(
-        (_) => Future.value(base64Encode(identityKeyPair.serialize())));
-    when(secureStorage.read(key: 'registrationId'))
-        .thenAnswer((_) => Future.value(registrationId.toString()));
+      when(secureStorage.read(key: 'identityKeyPair')).thenAnswer(
+          (_) => Future.value(base64Encode(identityKeyPair.serialize())));
+      when(secureStorage.read(key: 'registrationId'))
+          .thenAnswer((_) => Future.value(registrationId.toString()));
 
-    expect(await secureStorage.read(key: 'identityKeyPair'), isNotNull);
-    expect(await secureStorage.read(key: 'identityKeyPair'),
-        base64Encode(identityKeyPair.serialize()));
-    expect(await secureStorage.read(key: 'registrationId'), isNotNull);
-    expect(await secureStorage.read(key: 'registrationId'),
-        registrationId.toString());
-  });
+      expect(await secureStorage.read(key: 'identityKeyPair'), isNotNull);
+      expect(await secureStorage.read(key: 'identityKeyPair'),
+          base64Encode(identityKeyPair.serialize()));
+      expect(await secureStorage.read(key: 'registrationId'), isNotNull);
+      expect(await secureStorage.read(key: 'registrationId'),
+          registrationId.toString());
+    });
 
-  test('getIdentity should return the correct identity', () async {
-    final secureStorage = MockSecureStorage();
-    final identityKeyPair = generateIdentityKeyPair();
-    final registrationId = generateRegistrationId(false);
-    final identityKeyStore = SecureStorageIdentityKeyStore.fromIdentityKeyPair(
-        secureStorage, identityKeyPair, registrationId);
+    test('getIdentity should return the correct identity', () async {
+      final secureStorage = MockSecureStorage();
+      final identityKeyPair = generateIdentityKeyPair();
+      final registrationId = generateRegistrationId(false);
+      final identityKeyStore =
+          SecureStorageIdentityKeyStore.fromIdentityKeyPair(
+              secureStorage, identityKeyPair, registrationId);
 
-    const address = SignalProtocolAddress('test', 1);
-    final identityKey = identityKeyPair.getPublicKey();
-    final identityKeyString = base64Encode(identityKey.serialize());
+      const address = SignalProtocolAddress('test', 1);
+      final identityKey = identityKeyPair.getPublicKey();
+      final identityKeyString = base64Encode(identityKey.serialize());
 
-    when(secureStorage.read(key: address.toString()))
-        .thenAnswer((_) => Future.value(identityKeyString));
+      when(secureStorage.read(key: address.toString()))
+          .thenAnswer((_) => Future.value(identityKeyString));
 
-    expect(await identityKeyStore.getIdentity(address), isNotNull);
-    expect(await identityKeyStore.getIdentity(address), identityKey);
-  });
+      expect(await identityKeyStore.getIdentity(address), isNotNull);
+      expect(await identityKeyStore.getIdentity(address), identityKey);
+    });
 
-  test('getIdentityKeyPair should return the correct identity key pair',
-      () async {
-    final secureStorage = MockSecureStorage();
-    final identityKeyPair = generateIdentityKeyPair();
-    final registrationId = generateRegistrationId(false);
-    final identityKeyStore = SecureStorageIdentityKeyStore.fromIdentityKeyPair(
-        secureStorage, identityKeyPair, registrationId);
+    test('getIdentityKeyPair should return the correct identity key pair',
+        () async {
+      final secureStorage = MockSecureStorage();
+      final identityKeyPair = generateIdentityKeyPair();
+      final registrationId = generateRegistrationId(false);
+      final identityKeyStore =
+          SecureStorageIdentityKeyStore.fromIdentityKeyPair(
+              secureStorage, identityKeyPair, registrationId);
 
-    final identityKeyPairString = base64Encode(identityKeyPair.serialize());
+      final identityKeyPairString = base64Encode(identityKeyPair.serialize());
 
-    when(secureStorage.read(key: 'identityKeyPair'))
-        .thenAnswer((_) => Future.value(identityKeyPairString));
+      when(secureStorage.read(key: 'identityKeyPair'))
+          .thenAnswer((_) => Future.value(identityKeyPairString));
 
-    expect(await identityKeyStore.getIdentityKeyPair(), isNotNull);
-    // testing serialized values because the object references are different
-    expect((await identityKeyStore.getIdentityKeyPair()).serialize(),
-        identityKeyPair.serialize());
-  });
+      expect(await identityKeyStore.getIdentityKeyPair(), isNotNull);
+      // testing serialized values because the object references are different
+      expect((await identityKeyStore.getIdentityKeyPair()).serialize(),
+          identityKeyPair.serialize());
+    });
 
-  test('getLocalRegistrationId should return the correct registration ID',
-      () async {
-    final secureStorage = MockSecureStorage();
-    final identityKeyPair = generateIdentityKeyPair();
-    final registrationId = generateRegistrationId(false);
-    final identityKeyStore = SecureStorageIdentityKeyStore.fromIdentityKeyPair(
-        secureStorage, identityKeyPair, registrationId);
+    test('getLocalRegistrationId should return the correct registration ID',
+        () async {
+      final secureStorage = MockSecureStorage();
+      final identityKeyPair = generateIdentityKeyPair();
+      final registrationId = generateRegistrationId(false);
+      final identityKeyStore =
+          SecureStorageIdentityKeyStore.fromIdentityKeyPair(
+              secureStorage, identityKeyPair, registrationId);
 
-    when(secureStorage.read(key: 'registrationId'))
-        .thenAnswer((_) => Future.value(registrationId.toString()));
+      when(secureStorage.read(key: 'registrationId'))
+          .thenAnswer((_) => Future.value(registrationId.toString()));
 
-    expect(await identityKeyStore.getLocalRegistrationId(), isNotNull);
-    expect(await identityKeyStore.getLocalRegistrationId(), registrationId);
-  });
+      expect(await identityKeyStore.getLocalRegistrationId(), isNotNull);
+      expect(await identityKeyStore.getLocalRegistrationId(), registrationId);
+    });
 
-  test('isTrustedIdentity should return true if identity is trusted', () async {
-    final secureStorage = MockSecureStorage();
-    final identityKeyPair = generateIdentityKeyPair();
-    final registrationId = generateRegistrationId(false);
-    final identityKeyStore = SecureStorageIdentityKeyStore.fromIdentityKeyPair(
-        secureStorage, identityKeyPair, registrationId);
+    test('isTrustedIdentity should return true if identity is trusted',
+        () async {
+      final secureStorage = MockSecureStorage();
+      final identityKeyPair = generateIdentityKeyPair();
+      final registrationId = generateRegistrationId(false);
+      final identityKeyStore =
+          SecureStorageIdentityKeyStore.fromIdentityKeyPair(
+              secureStorage, identityKeyPair, registrationId);
 
-    const address = SignalProtocolAddress('test', 1);
-    final identityKey = identityKeyPair.getPublicKey();
-    final identityKeyString = base64Encode(identityKey.serialize());
+      const address = SignalProtocolAddress('test', 1);
+      final identityKey = identityKeyPair.getPublicKey();
+      final identityKeyString = base64Encode(identityKey.serialize());
 
-    when(secureStorage.read(key: address.toString()))
-        .thenAnswer((_) => Future.value(identityKeyString));
+      when(secureStorage.read(key: address.toString()))
+          .thenAnswer((_) => Future.value(identityKeyString));
 
-    expect(
-        await identityKeyStore.isTrustedIdentity(
-            address, identityKey, Direction.receiving),
-        isTrue);
-  });
+      expect(
+          await identityKeyStore.isTrustedIdentity(
+              address, identityKey, Direction.receiving),
+          isTrue);
+    });
 
-  test('isTrustedIdentity should return false if identity is not trusted',
-      () async {
-    final secureStorage = MockSecureStorage();
-    final identityKeyPair = generateIdentityKeyPair();
-    final registrationId = generateRegistrationId(false);
-    final identityKeyStore = SecureStorageIdentityKeyStore.fromIdentityKeyPair(
-        secureStorage, identityKeyPair, registrationId);
+    test('isTrustedIdentity should return false if identity is not trusted',
+        () async {
+      final secureStorage = MockSecureStorage();
+      final identityKeyPair = generateIdentityKeyPair();
+      final registrationId = generateRegistrationId(false);
+      final identityKeyStore =
+          SecureStorageIdentityKeyStore.fromIdentityKeyPair(
+              secureStorage, identityKeyPair, registrationId);
 
-    const address = SignalProtocolAddress('test', 1);
-    final identityKey = identityKeyPair.getPublicKey();
-    final identityKeyString = base64Encode(identityKey.serialize());
+      const address = SignalProtocolAddress('test', 1);
+      final identityKey = identityKeyPair.getPublicKey();
+      final identityKeyString = base64Encode(identityKey.serialize());
 
-    when(secureStorage.read(key: address.toString()))
-        .thenAnswer((_) => Future.value(identityKeyString));
+      when(secureStorage.read(key: address.toString()))
+          .thenAnswer((_) => Future.value(identityKeyString));
 
-    final newIdentityKeyPair = generateIdentityKeyPair();
-    final newIdentityKey = newIdentityKeyPair.getPublicKey();
+      final newIdentityKeyPair = generateIdentityKeyPair();
+      final newIdentityKey = newIdentityKeyPair.getPublicKey();
 
-    expect(
-        await identityKeyStore.isTrustedIdentity(
-            address, newIdentityKey, Direction.receiving),
-        isFalse);
-  });
+      expect(
+          await identityKeyStore.isTrustedIdentity(
+              address, newIdentityKey, Direction.receiving),
+          isFalse);
+    });
 
-  test('saveIdentity should save the identity key', () async {
-    final secureStorage = MockSecureStorage();
-    final identityKeyPair = generateIdentityKeyPair();
-    final registrationId = generateRegistrationId(false);
-    final identityKeyStore = SecureStorageIdentityKeyStore.fromIdentityKeyPair(
-        secureStorage, identityKeyPair, registrationId);
+    test('saveIdentity should save the identity key', () async {
+      final secureStorage = MockSecureStorage();
+      final identityKeyPair = generateIdentityKeyPair();
+      final registrationId = generateRegistrationId(false);
+      final identityKeyStore =
+          SecureStorageIdentityKeyStore.fromIdentityKeyPair(
+              secureStorage, identityKeyPair, registrationId);
 
-    const address = SignalProtocolAddress('test', 1);
-    final identityKey = identityKeyPair.getPublicKey();
-    final identityKeyString = base64Encode(identityKey.serialize());
+      const address = SignalProtocolAddress('test', 1);
+      final identityKey = identityKeyPair.getPublicKey();
+      final identityKeyString = base64Encode(identityKey.serialize());
 
-    when(secureStorage.read(key: address.toString()))
-        .thenAnswer((_) => Future.value(identityKeyString));
+      when(secureStorage.read(key: address.toString()))
+          .thenAnswer((_) => Future.value(identityKeyString));
 
-    final newIdentityKeyPair = generateIdentityKeyPair();
-    final newIdentityKey = newIdentityKeyPair.getPublicKey();
+      final newIdentityKeyPair = generateIdentityKeyPair();
+      final newIdentityKey = newIdentityKeyPair.getPublicKey();
 
-    expect(
-        await identityKeyStore.saveIdentity(address, newIdentityKey), isTrue);
+      expect(
+          await identityKeyStore.saveIdentity(address, newIdentityKey), isTrue);
+    });
   });
 }
