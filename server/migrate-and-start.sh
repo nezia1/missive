ded659f85daca75f947b6ee957ddd0b3e8593226
Anthony Rodriguez <anthony@nezia.dev>
2024-05-21 15:51:04 +0200

    add google application credentials when using swarm

--- server/migrate-and-start.sh
+++ server/migrate-and-start.sh
@@ -5,6 +5,7 @@ if [ "$NODE_ENV" = "production" ]; then
     export COOKIE_SECRET=$(cat /run/secrets/cookie_secret)
     export PRIVATE_KEY_PATH=/run/secrets/private_key
     export PUBLIC_KEY_PATH=/run/secrets/public_key
+    export GOOGLE_APPLICATION_CREDENTIALS=/run/secrets/google_application_credentials
     npm start
 else 
     npx prisma migrate dev 
