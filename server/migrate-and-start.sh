431e71224d9d266bd86ca30b2b583679fbf67670
Anthony Rodriguez <anthony@nezia.dev>
2024-05-21 11:59:11 +0200

    update docker-compose to be more flexible and standard (also hot reload on dev)

--- server/migrate-and-start.sh
+++ server/migrate-and-start.sh
@@ -1,8 +1,9 @@
 #!/bin/sh 
-if [ "$NODE_ENV" == "production" ]; then
+if [ "$NODE_ENV" = "production" ]; then
     npx prisma migrate deploy
+    npm start
 else 
     npx prisma migrate dev 
+    npm run dev
 fi
-npm start
 
