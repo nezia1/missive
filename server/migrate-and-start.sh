5e7f01aea5468230d509253ba6333f093d4e84c0
Anthony Rodriguez <anthony@nezia.dev>
2024-05-28 10:44:19 +0200

    remove debug logs

--- server/migrate-and-start.sh
+++ server/migrate-and-start.sh
@@ -6,7 +6,6 @@ if [ "$NODE_ENV" = "production" ]; then
     export PRIVATE_KEY_PATH=/run/secrets/private_key
     export PUBLIC_KEY_PATH=/run/secrets/public_key
     export GOOGLE_APPLICATION_CREDENTIALS=/run/secrets/google_application_credentials
-    echo "$DATABASE_URL"
     npm start
 else 
     npx prisma migrate dev 
