9c36317d9ea85eef825c28e1f354a1160ae214bb
Anthony Rodriguez <anthony@nezia.dev>
2024-05-30 13:12:44 +0200

    comment more signal implementation

--- client/lib/features/encryption/namespaced_secure_storage.dart
+++ client/lib/features/encryption/namespaced_secure_storage.dart
@@ -2,6 +2,15 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 /// A wrapper around [FlutterSecureStorage] that allows for namespacing keys.
 /// This is used when we want to store multiple keys, but want to avoid key collisions.
+///
+/// ## Usage
+/// ```dart
+/// final namespacedSecureStorage = NamespacedSecureStorage(
+///   secureStorage: FlutterSecureStorage(),
+///   namespace: 'myNamespace',
+/// );
+/// await namespacedSecureStorage.write(key: 'myKey', value: 'myValue');
+/// ```
 class NamespacedSecureStorage implements SecureStorage {
   final FlutterSecureStorage _secureStorage;
   final String? _namespace;
@@ -11,30 +20,55 @@ class NamespacedSecureStorage implements SecureStorage {
       : _secureStorage = secureStorage,
         _namespace = namespace;
 
-  // Returns the value associated with the given key.
   @override
+
+  /// Returns the value associated with the given key.
+  ///
+  /// ## Parameters
+  /// - `key`: The key whose associated value is to be returned.
+  ///
+  /// ## Returns
+  /// - The value associated with the given key, or `null` if the key does not exist.
   Future<String?> read({required String key}) async {
     return await _secureStorage.read(key: _getNamespacedKey(key));
   }
 
-  // Writes the given value to the given key.
   @override
+
+  /// Writes the given value to the given key.
+  ///
+  /// ## Parameters
+  /// - `key`: The key to write the value to.
+  /// - `value`: The value to be written.
   Future<void> write({required String key, required String? value}) async {
     await _secureStorage.write(key: _getNamespacedKey(key), value: value);
   }
 
   @override
+
+  /// Deletes the value associated with the given key.
+  ///
+  /// ## Parameters
+  /// - `key`: The key whose value is to be deleted.
   Future<void> delete({required String key}) async {
     await _secureStorage.delete(key: _getNamespacedKey(key));
   }
 
   @override
+
+  /// Deletes all keys and values from the secure storage.
   Future<void> deleteAll() async {
     await _secureStorage.deleteAll();
   }
 
   /// Returns the key with the namespace prepended.
   /// If no namespace is set, the key is returned as is.
+  ///
+  /// ## Parameters
+  /// - `key`: The key to be namespaced.
+  ///
+  /// ## Returns
+  /// - The namespaced key.
   String _getNamespacedKey(String key) {
     return '${_namespace != null ? '${_namespace}_' : ''}$key';
   }
--- client/lib/features/encryption/secure_storage_signed_pre_key_store.dart
+++ client/lib/features/encryption/secure_storage_signed_pre_key_store.dart
@@ -2,6 +2,17 @@ import 'dart:convert';
 import 'package:libsignal_protocol_dart/libsignal_protocol_dart.dart';
 import 'package:missive/features/encryption/namespaced_secure_storage.dart';
 
+/// A class that implements [SignedPreKeyStore] to store, retrieve, and manage signed pre keys using [SecureStorage].
+///
+/// This class interacts with the device's secure storage to handle signed pre keys,
+/// which are essential for the Signal Protocol. It provides methods to check the existence,
+/// load, store, and remove signed pre keys, as well as a private method to load all keys.
+///
+/// ## Usage
+/// ```dart
+/// final secureStorageSignedPreKeyStore = SecureStorageSignedPreKeyStore(secureStorage);
+/// await secureStorageSignedPreKeyStore.storeSignedPreKey(signedPreKeyId, signedPreKeyRecord);
+/// ```
 class SecureStorageSignedPreKeyStore implements SignedPreKeyStore {
   final SecureStorage _secureStorage;
 
@@ -9,6 +20,14 @@ class SecureStorageSignedPreKeyStore implements SignedPreKeyStore {
       : _secureStorage = secureStorage;
 
   @override
+
+  /// Checks if a signed pre key with the given [signedPreKeyId] exists in the secure storage.
+  ///
+  /// ## Parameters
+  /// - `signedPreKeyId`: The ID of the signed pre key to check.
+  ///
+  /// ## Returns
+  /// - `true` if the signed pre key exists, `false` otherwise.
   Future<bool> containsSignedPreKey(int signedPreKeyId) async {
     final signedPreKeys = await _loadKeys();
 
@@ -18,6 +37,17 @@ class SecureStorageSignedPreKeyStore implements SignedPreKeyStore {
   }
 
   @override
+
+  /// Loads a serialized [SignedPreKeyRecord] from [SecureStorage] and returns it as a [SignedPreKeyRecord].
+  ///
+  /// ## Parameters
+  /// - `signedPreKeyId`: The ID of the signed pre key to load.
+  ///
+  /// ## Returns
+  /// - A [SignedPreKeyRecord] if found.
+  ///
+  /// ## Throws
+  /// - `InvalidKeyIdException` if the signed pre key is not found.
   Future<SignedPreKeyRecord> loadSignedPreKey(int signedPreKeyId) async {
     final signedPreKeys = await _loadKeys();
     if (signedPreKeys == null ||
@@ -29,18 +59,28 @@ class SecureStorageSignedPreKeyStore implements SignedPreKeyStore {
   }
 
   @override
+
+  /// Loads all serialized [SignedPreKeyRecord]s from [SecureStorage] and returns them as a list of [SignedPreKeyRecord]s.
+  ///
+  /// ## Returns
+  /// - A list of [SignedPreKeyRecord]s if found, an empty list otherwise.
   Future<List<SignedPreKeyRecord>> loadSignedPreKeys() async {
     final signedPreKeys = await _loadKeys();
 
     if (signedPreKeys == null) return [];
 
-    // each element is serialized
+    // Each element is serialized
     return signedPreKeys.values
         .map((value) => SignedPreKeyRecord.fromSerialized(base64Decode(value)))
         .toList();
   }
 
   @override
+
+  /// Removes a signed pre key with the given [signedPreKeyId] from the secure storage.
+  ///
+  /// ## Parameters
+  /// - `signedPreKeyId`: The ID of the signed pre key to remove.
   Future<void> removeSignedPreKey(int signedPreKeyId) async {
     final signedPreKeys = await _loadKeys();
 
@@ -53,6 +93,14 @@ class SecureStorageSignedPreKeyStore implements SignedPreKeyStore {
   }
 
   @override
+
+  /// Stores a [SignedPreKeyRecord] in the device's secure storage.
+  ///
+  /// The signed pre key is stored as a base64 encoded [String].
+  ///
+  /// ## Parameters
+  /// - `signedPreKeyId`: The ID of the signed pre key to store.
+  /// - `record`: The [SignedPreKeyRecord] to store.
   Future<void> storeSignedPreKey(
       int signedPreKeyId, SignedPreKeyRecord record) async {
     var signedPreKeys = await _loadKeys();
@@ -65,7 +113,10 @@ class SecureStorageSignedPreKeyStore implements SignedPreKeyStore {
         key: 'signedPreKeys', value: jsonEncode(signedPreKeys));
   }
 
-  /// Load signed pre-keys from secure storage as base 64 encoded [SignedPreKeyRecord]s.
+  /// Loads signed pre keys from secure storage as base64 encoded [SignedPreKeyRecord]s.
+  ///
+  /// ## Returns
+  /// - A [Map] of signed pre keys if found, `null` otherwise.
   Future<Map<String, dynamic>?> _loadKeys() async {
     final preKeysString =
         await _secureStorage.read(key: 'signedPreKeys').catchError((_) => null);
