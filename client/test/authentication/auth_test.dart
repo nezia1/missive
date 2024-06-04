e42cef6eeef9edf4a05cf5ca91bfc5962f9f9981
Anthony Rodriguez <anthony@nezia.dev>
2024-06-04 12:11:51 +0200

    move auth test to missive

---
+++ client/test/authentication/auth_test.dart
@@ -0,0 +1,54 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:missive/features/authentication/providers/auth_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:mockito/mockito.dart';
+import 'auth_test.mocks.dart';
+
+@GenerateMocks([Dio])
+@GenerateMocks([FlutterSecureStorage])
+void main() {
+  group('AuthProvider', () {
+    final httpClient = MockDio();
+    final secureStorage = MockFlutterSecureStorage();
+    var userProvider =
+        AuthProvider(httpClient: httpClient, secureStorage: secureStorage);
+    SharedPreferences.setMockInitialValues({});
+    test('AuthProvider should return a null user when instantiated', () async {
+      expect(await userProvider.user, isNull);
+    });
+
+    test('AuthProvider should return a null access token when instantiated',
+        () async {
+      expect(await userProvider.accessToken, isNull);
+    });
+
+    test('AuthProvider should return a false isLoggedIn when instantiated',
+        () async {
+      expect(userProvider.isLoggedIn, false);
+    });
+
+    test(
+        'login should return an AuthenticationSuccess when the login is successful',
+        () async {
+      WidgetsFlutterBinding.ensureInitialized();
+
+      // Mock a successful login attempt
+      when(httpClient.post('/tokens', data: anyNamed('data')))
+          .thenAnswer((_) async => Response(
+              requestOptions: RequestOptions(path: '/api/v1/tokens'),
+              data: {
+                'data': {'status': 'success', 'accessToken': 'token'}
+              },
+              statusCode: 200));
+
+      final result = await userProvider.login('user', 'password');
+
+      expect(result, isA<AuthenticationSuccess>());
+    });
+  });
+}
